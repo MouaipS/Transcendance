ea6ebaa502fb02619d3705570d00ceb38de84ff0
adrouin <amaury.d.pro@proton.me>
2026-03-30 16:37:38 +0200

    [REFACTO] frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+//import Profile from './pages/Profile.jsx';
+
+
+function App() {
+    return (
+        <div>
+            <h1>Transcendance</h1>
+        </div>
+    );
+}
+
+export default App;
