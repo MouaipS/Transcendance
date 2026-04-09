b97f0684bab5fe4611d26e40b23e201ab45fbd6d
Tom Bourgeon <tobourge@3A4.42angouleme.fr>
2026-04-09 11:26:59 +0200

    [ADD] test prisma requests

--- backend/src/route/linker.ts
+++ backend/src/route/linker.ts
@@ -1,10 +1,23 @@
-import Fastify, { FastifyInstance, FastifyPluginAsync } from 'fastify' //This imports the Fastify object types we need, its only for variable types
+import Fastify, { FastifyInstance, FastifyRequest, FastifyReply, FastifyPluginAsync } from 'fastify' //This imports the Fastify object types we need, its only for variable types
 import {loginRoute} from "../route/authentication/login.js" //this the import for loginRoute function that is in the login.js
 import {registerRoute} from "../route/authentication/register.js"//same but for the register route. NOTE : the path start from the main which is index.ts
 
 //This linker function takes the server as parameter which is a FastifyInstance type (our server)
 export async function linker(server: FastifyInstance)
 {
-	server.post('/api/login', async(request, reply) => { loginRoute(request, reply);}) //these are the shorthand route declaration that execute the actual route
-	server.post('/api/register', async(request, reply) => { registerRoute(request, reply);})
+	//server.post('/api/login', async(request, reply) => { loginRoute(request, reply);}) //these are the shorthand route declaration that execute the actual route
+	server.post('/api/login', async (request, reply) => { loginRoute(request, reply)});
+	server.post<{Body: any}>('/api/register', async (request, reply) => { registerRoute(request, reply)});
 }
+
+//REQUETES TEST 
+
+// REGISTER :
+// curl -X POST http://localhost:3000/api/register \
+//      -H "Content-Type: application/json" \
+//      -d '{"username": "testuser", "password": "mypassword123"}'
+
+// LOGIN :
+// curl -X POST http://localhost:3000/api/login \
+//      -H "Content-Type: application/json" \
+//      -d '{"username": "testuser", "password": "mypassword123"}'
