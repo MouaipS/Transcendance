d99aa2483215d9dfc4ecd58bd1cd1c19677ad7f9
adrouin <amaury.d.pro@proton.me>
2026-04-09 16:52:12 +0200

    Vault [ADD] function to access Vault information

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,25 @@
-import Fastify from 'fastify'
+import Fastify 			from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import bcrypt			from 'bcrypt'
+
+
+/**
+ * @brief demande au vault les variables au vault
+ * @param auth (info pour l'authentification des users) et database (pour la db)
+ * @returns un json avec les infos
+ */
+async function fetchVault(path) {
+	const response = await fetch(`${VAULT_ADDR}/v1/secret/data/path`, {
+		headers: {
+			"X-Vault-Token": VAULT_TOKEN
+		}
+	});
+	if (response.ok === false)
+		throw new Error("Vault information is unavailable");
+	const json = await response.json();
+	return json.data.data;
+}
+
 
 const fastify = Fastify({ logger: true });
 
