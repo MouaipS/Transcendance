da2d45642e1a12e52f842b652a0eab4bd7ad2d33
Tom Bourgeon <tobourge@3C4.42angouleme.fr>
2026-04-21 17:06:58 +0200

    lobby [ADD] : route de creation avec generation de code, route de join avec validation du code

--- backend/src/route/linker.ts
+++ backend/src/route/linker.ts
@@ -3,8 +3,7 @@ import {loginRoute} from "../route/authentication/login.js" //this the import fo
 import {registerRoute} from "../route/authentication/register.js"//same but for the register route. NOTE : the path start from the main which is index.ts
 import {logoutRoute} from "../route/authentication/logout.js"
 import {homePageRoute} from "../route/pages/homePage.js"
-import { joinRoute } from './game/joins.js'
-import { gameCreationRoute } from './game/create.js'
+import { joinGameRoute , createGameRoute } from './game/lobby.js'
 
 
 //This linker function takes the server as parameter which is a FastifyInstance type (our server)
@@ -28,10 +27,11 @@ export async function linker(server: FastifyInstance)
 
 	server.decorate("authenticate", authenticate);
 
-	server.post<{Body: any}>('/api/login', async (request, reply) => { return loginRoute(request, reply)});//these are the shorthand route declaration that execute the actual route
-	server.post<{Body: any}>('/api/register', async (request, reply) => { return registerRoute(request, reply)});
-	server.post('/api/logout', async (request, reply) => { return logoutRoute(reply)});
-	server.get('/api/home', {onRequest : [authenticate]}, async (request, reply) => { return homePageRoute(request, reply)});
-	server.get('/api/game/join', async (request, reply) => { return joinRoute(request, reply)});
-	server.post<{Body:any}>('/api/game/create', async (request, reply) => { return gameCreationRoute(request, reply)});
+	//these are the shorthand route declaration that execute the actual route
+	server.post<{Body: any}>('/api/login', async (request, reply) => { return loginRoute(request, reply)}); 					//login route
+	server.post<{Body: any}>('/api/register', async (request, reply) => { return registerRoute(request, reply)}); 				// registration route
+	server.post('/api/logout', async (request, reply) => { return logoutRoute(reply)}); 										// logout route
+	server.get('/api/home', {onRequest : [authenticate]}, async (request, reply) => { return homePageRoute(request, reply)});	// homePage display route
+	server.post<{Body:any}>('/api/game/join', async (request, reply) => { return joinGameRoute(request, reply)});								// public random game route
+	server.post<{Body:any}>('/api/game/create', async (request, reply) => { return createGameRoute(request, reply)});			// private game creation route
 }
