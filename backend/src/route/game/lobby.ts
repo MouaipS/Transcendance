6d2f42257a04fea827e5477e662caf8719a86a03
Tom Bourgeon <tobourge@3A3.42angouleme.fr>
2026-04-22 16:23:41 +0200

    lobby [ADD] : creation of two arrays Lobbies[] and Games[], update of public Lobby (Lobby[0])

--- backend/src/route/game/lobby.ts
+++ backend/src/route/game/lobby.ts
@@ -1,10 +1,22 @@
 import {FastifyInstance, FastifyRequest, FastifyReply} from 'fastify'
 import { prisma } from "../../server/prisma.js"
 
-enum GameStatus {
-  WAITING,
-  PLAYING,
-}
+/**
+ * Fonctionnement des Lobbies :
+ * 
+ * Les games sont stockées dans deux tableaux, Lobbies[] et Games[]
+ * 
+ * Lobbies[] stocke toutes les games en attente de lancement, la première
+ * case Lobbies[0] est réservée a la Game publique qu'on rejoint en
+ * cliquant sur le bouton Join Public Game. Le reste sont les Game créees
+ * par les utilisateurs via la génération d'un code aléatoire. 
+ * Dès qu'une game est ready (4 joueurs), elle est lancée et push dans
+ * le tableau Games[]
+ * 
+ * Games[] stocke toutes les games en cours. Lorsqu'une game se termine,
+ * un objet statistiques est envoyé a la base de données pour être stocké
+ */
+
 
 interface CreationRequestBody {
   username: string;
@@ -15,22 +27,27 @@ interface JoinRequestBody {
   code: string;
 }
 
-interface Game {
+interface Lobby {
   owner: string;
-  status: GameStatus;
   code: string;
   nb_players: number;
   users: string[];
 }
 
-//Creation of games objects array : first element is dedicated to the public room
-const games: Game[] = [{
+interface Game {
+  owner: string;
+  users: string[];
+}
+
+//Creation of arrays Lobbies[] (Game waiting) and Games[] (Games running)
+const lobbies: Lobby[] = [{
   owner: "public",
-  status: GameStatus.WAITING,
   code: '',
   nb_players: 0,
-  users: []
+  users: ["Player1", "Player2", "Player3", "Player4"]
 }];
+
+const games: Game[] = []
 
 
 function generateGameCode(): string {
@@ -52,59 +69,63 @@ function generateGameCode(): string {
 
 function codeAlreadyExists(code: string): boolean
 {
-  return games.some(game => game.code === code)
+  return lobbies.some(lobby => lobby.code === code)
 }
 
 //Route for private game creation : code generation et setup game variables
-// body : {username}
+//  receive body : {username}
+//  reply game : {owner, status, code, nb_players, users[]}
 export async function createGameRoute(request : FastifyRequest<{Body: CreationRequestBody}>, reply : FastifyReply)
 {
-  console.log("createGame Request")
   const {username} = request.body
   let code: string = ''
   while (code === '' || codeAlreadyExists(code)) {
     code = generateGameCode()
   }
 
-  const game: Game = {owner: username, status: GameStatus.WAITING, code: code, nb_players: 1, users: [username]}
-  games.push(game)
-  console.log(games)
-  return reply.status(200).send(game);
+  const lobby: Lobby = {owner: username, code: code, nb_players: 1, users: [username, "Player2", "Player3", "Player4"]}
+  lobbies.push(lobby)
+  console.log("LOBBIES", lobbies)
+  console.log("GAMES", games)
+  return reply.status(200).send(lobby);
 }
 
 
-function joinGame(game: Game, username: string)
+function joinLobby(lobby: Lobby, username: string)
 {
-  game.nb_players += 1
-  game.users.push(username)
+  lobby.nb_players += 1
+  lobby.users[lobby.nb_players - 1] = username
+  if (lobby.nb_players === 4)
+  {
+    const index = lobbies.findIndex(lob => lob === lobby)
+    if (index === 0)
+    {
+      const newLobby: Lobby = {owner: "public", code: '', nb_players: 0, users: ["Player1", "Player2", "Player3", "Player4"]}
+      lobbies.splice(index, 1, newLobby)
+    }
+    else
+      lobbies.splice(index, 1)
+    games.push(lobby)
+  }
+  console.log("LOBBIES", lobbies)
+  console.log("GAMES", games)
 }
+
 
 //Route to join a private game : comparison of code
-// body : {username, code}
+//  receive body : {username, code}
+//  reply game : {owner, status, code, nb_players, users[]}
 export async function joinGameRoute(request : FastifyRequest<{Body: JoinRequestBody}>, reply : FastifyReply)
 {
-  console.log(games)
   const {username, code} = request.body
-  if(!code)                //join public game
+  if(!code && lobbies[0].nb_players < 4)                //join public game
   {
-    joinGame(games[0], username)
+    joinLobby(lobbies[0], username)
     return reply.status(200).send(games[0]);
   }
-  const game = games.find(game => game.code === code)
-  if (!game)
-  {
-    console.log("error: Invalid Code")
-    return reply.status(404).send({ error: "Invalid code" })
-  }
-  else if (game && game.status === GameStatus.PLAYING)
-  {
-    console.log("error: The game has already started")
-    return reply.status(404).send({ error: "The game has already started" })
-  }
-  else
-  {
-    joinGame(game, username)
-    console.log(game)
-    return reply.status(200).send(game);
-  }
+  const lobby = lobbies.find(lobby => lobby.code === code)
+  if (!lobby)
+    return reply.status(404).send({ error: "Game unavailable" })
+  joinLobby(lobby, username)
+  return reply.status(200).send(lobby);
 }
