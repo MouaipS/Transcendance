0aa91f1a4dcfee89ce404e82d149b78e07911ddd
adrouin <amaury.d.pro@proton.me>
2026-05-05 18:06:54 +0200

    [ADD] RulesCard ex et Recipescard ex

--- frontend/src/components/data/cards.jsx
+++ frontend/src/components/data/cards.jsx
@@ -1,8 +1,16 @@
 
 
 const RulesCard = () => (
-	<div className="text-gray-500 italic">
-		Stats en cours de développement...
+	<div className="text-sm space-y-2">
+		<p>Le but : <span className="font-bold">accumuler le plus de points</span> 
+			en remportant des plis.</p>
+		<p>Façons de gagner un pli :</p>
+		<ul className="list-disc ml-5 space-y-1">
+			<li>Smasher en cas de paire</li>
+			<li>Smasher en cas de sandwich</li>
+			<li>Smasher un combo</li>
+			<li>Remplir son récipient</li>
+		</ul>
 	</div>
 )
 
@@ -13,16 +21,35 @@ const StatsCard = () => (
 
 )
 
-const RecipesCard = () => (
-	<div className="text-gray-500 italic">
-		Stats en cours de développement...
-	</div>
+const RecipesCard = () => {
 
-)
+	const recipes = [
+		{ name: 'Recette A', ingredients: ['navet', 'carotte'] },
+		{ name: 'Recette B', ingredients: ['concombre', 'cheval'] },
+		{ name: 'Recette C', ingredients: ['poivron', 'oignon', 'tomate'] },
+	]
+
+	return <div className="space-y-3">
+		{recipes.map((r, i) => (
+			<div key ={i}>
+				<p className="font-bold text-lg">
+					{r.name} :
+				</p>
+				<ul className="ml-4 text-sm">
+					{r.ingredients.map((ing, j) => (
+						<li key={j}>
+							|→ {ing}
+						</li>
+					))}
+				</ul>
+			</div>
+		))}
+	</div>
+}
 
 const ChatCard = () => (
 	<div className="text-gray-500 italic">
-		Stats en cours de développement...
+		Chat en cours de développement...
 	</div>
 
 )
