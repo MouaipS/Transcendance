fee54f1229eb955e12567ea5aabc3766a625dfad
adrouin <amaury.d.pro@proton.me>
2026-05-05 16:13:01 +0200

    [ADD] changer la fiche 1

--- frontend/src/components/data/cards.jsx
+++ frontend/src/components/data/cards.jsx
@@ -3,8 +3,24 @@
 //tbleau avec les fiches de la pile
 const cardsData = [
 	{
-		title: "test nom",
-		content: <p>contenu</p>,
+		title: "test 1",
+		content: <p>contenu 1</p>,
+	},
+	{
+		title: "test 2",
+		content: <p>contenu 2</p>,
+	},
+	{
+		title: "test 3",
+		content: <p>contenu 3</p>,
+	},
+	{
+		title: "test 4",
+		content: <p>contenu 4</p>,
+	},
+	{
+		title: "test 5",
+		content: <p>contenu 5</p>,
 	}
 ]
 
--- frontend/src/components/tools/CardStack.jsx
+++ frontend/src/components/tools/CardStack.jsx
@@ -1,21 +1,32 @@
 import { cardsData } from "../data/cards";
+import { useState } from "react"
 
 export default function CardStack () {
-	const card = cardsData[0];
+	const [ activeIndex, setActiveIndex ] = useState(0)
+	const total = cardsData.length
+	const card = cardsData[activeIndex]
+
+	const nextCard = () => setActiveIndex((prev) => (prev + 1) % total)
+	const prevCard = () => setActiveIndex((prev) => (prev -1 + total )% total)
 
 	return <div className="relative max-w-87.5 mt-4">
 
 			{/**les fiches en fond */}
-			{[1, 2, 3].map((index) => (
+			{[1, 2, 3].map((index) => {
+				const idx = (activeIndex + index) % total
+				return (
 				<div 
 					key={index}
-					className="absolute inset-0 border border-slate-400 bg-white"
+					onClick={() => setActiveIndex(idx)}
+					className="absolute inset-0 border border-slate-400 bg-white cursor-pointer hover:bg-slate-50"
 					style={{
 						transform: `translate(${index * 6}px, ${index * 6}px)`,
 						zIndex: 10 - index,
 					}}
+					arial-label={`Aller à la fiche ${cardsData[idx].title}`}
 					/>
-			))}
+			)
+	})}
 
 			{/**fiche premier plan */}
 			<div
