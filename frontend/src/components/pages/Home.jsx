626bd5c98c51e26cd34292b724cb6ff32e1b7366
adrouin <amaury.d.pro@proton.me>
2026-05-06 10:37:55 +0200

    [REFACTO] de la page avec column à gauche et large bloc à droite

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -61,53 +61,32 @@ export function Home () {
 
 	return <>
 	<div className="relative w-full h-screen overflow-hidden">
-		<div
-			className="absolute inset-0 bg-cover bg-center"
-			//style={{ backgroundImage: `url(${michelImg})` }}
-		>
-			<div className="gap-50 flex">
-				<div className="absolute flex flex-col left-4">
-					<h2 
-            className="font-serif italic mt-10 mb-2 
-            text-center text-2xl/9 font-bold tracking-tight 
-            text-black">VegeBattle FruitFight bagarre</h2>
 
-					<div dir="ltr">
-						<button
-							onClick={() => navigate('/profile')}
-							className="flex items-center gap-6 rounded-md 
-							border border-black shadow-md bg-white 
-							text-sm font-medium text-gray-700 hover:bg-gray-50 
-							focus:outline-none mt-5 min-w-40"
-						>
-							<img
-								alt="avatar par défaut"
-								src="src/components/images/default_avatar.webp"
-								className="h-14 w-14 object-cover rounded-s-lg"
-							/>
-							<span className="pr-4 text-2xl">{username}</span>
-						</button>
-					</div>
-
-					<CardStack/>
-				
-					<NavigateButtons/>
-          
-					<div>
-						<h2 className="font-serif text-xl">name: </h2>
-						<button className="bg-yellow-400 border border-black
-							shadow-md hover:shadow-none hover:inset-shadow-xs
-							hover:inset-shadow-black/50" onClick={logout}>Log out</button>
-					</div>
-
-          <div className="absolute min-w-340 min-h-200 rounded border-4 border-black mt-4 
-                flex flex-col left-120 top-10 max-h-200">
-            <Game/>
-          </div>
-
-        </div>
-			</div>
-		</div>
-	</div>
-	</>
+    <h1 className="font-serif italic text-center text-3xl font-bold tracking-tight text-black pt-6">
+      VEGEBATTLE
+    </h1>
+    <div className="flex gap-8 px-8 pt-6 h-[calc(100vh-100p)]">
+      <div className="flex flex-col w-90 shrink-0 min-h-0">
+        <button
+          onClick={() => navigate('/profile')}
+          className="flex items-center gap-6 rounded-md border border-black shadow-md bg-white hover:bg-gray-50">
+            <img
+              src="src/components/images/default_avatar.webp"
+              className="h-14 w-14 object-cover rounded-s-lg"/>
+            <span className="pr-4 text-2xl">{username}</span>
+        </button>
+        <CardStack/>
+        <button
+          className=" mt-6 self-start rounded-md bg-yellow-400 px-3 py-1.5 text-sm font-semibold
+                      border border-black shadow-md hover;bg-yellow-300"
+          onClick={logout}>
+            Log out
+        </button>
+      </div>
+      <div className="flex-1 rounded-md border-4 border-black overflow-hidden">
+        <Game/>
+      </div>
+    </div>
+  </div>
+</>
 }
